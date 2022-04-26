594d17e2594f1d557c38cf1735bc00431671bea6
Thuthuy1311 <72400802+Thuthuy1311@users.noreply.github.com>
2022-04-26 08:02:25 +0700

    Bai2

--- Bai2ngay26_04/Program.cs
+++ Bai2ngay26_04/Program.cs
@@ -10,7 +10,58 @@ namespace Bai2ngay26_04
     {
         static void Main(string[] args)
         {
+            Cat_266 cat = new Cat_266();
+            cat.makeSound_266();
+            cat.eat_266();
+            cat.run_266();
+            Bird_266 bird = new Bird_266();
+            bird.makeSound_266();
+            bird.eat_266();
+            bird.fly_266();
+            Console.ReadKey();
+        }
+    }
 
+    public abstract class Animal_266
+    {
+        public abstract void eat_266();
+
+        public abstract void makeSound_266();
+    }
+
+    public class Cat_266 : Animal_266
+    {
+        public override void makeSound_266()
+        {
+            Console.WriteLine("Cat say: meo meo ");
+        }
+
+        public override void eat_266()
+        {
+            Console.WriteLine("Cat eat: fish ");
+        }
+
+        public void run_266()
+        {
+            Console.WriteLine("Cat is running");
+        }
+    }
+
+    public class Bird_266 : Animal_266
+    {
+        public override void makeSound_266()
+        {
+            Console.WriteLine("Bird say: bip bip ");
+        }
+
+        public override void eat_266()
+        {
+            Console.WriteLine("Bird eat: Sau ");
+        }
+
+        public void fly_266()
+        {
+            Console.WriteLine("Bird is flying");
         }
     }
 }
