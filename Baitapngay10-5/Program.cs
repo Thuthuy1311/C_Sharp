c54c3f2232508fc7cddb9ca7ff9a295b3bc10863
Thuthuy1311 <72400802+Thuthuy1311@users.noreply.github.com>
2022-05-10 07:59:29 +0700

    nop bai tap ngay 10/5

--- Baitapngay10-5/Program.cs
+++ Baitapngay10-5/Program.cs
@@ -10,20 +10,125 @@ namespace Baitapngay10_5
     {
         static void Main(string[] args)
         {
-            int[] mang = new int[100];
-            //Nhap gia tri cho mang
-            for (int i = 1; i <= 10; i++)//Phan tu dau tien bat dau =0
+            int[] giatri_266 = new int[10];
+            for (int i = 0; i < 10; i++)
             {
-                Console.Write("Nhap so thu {0}:", i);
-                mang[i] = int.Parse(Console.ReadLine());
+                Console.Write("Nhap so thu " + i + " : ");
+                giatri_266[i] = int.Parse(Console.ReadLine());
             }
-            //Xuat gia tri da nhap
-            Console.WriteLine("Gia tri phan tu la:");
-            for (int i = 1; i <= 10; i++)//Phan tu dau tien bat dau =0
-            {
-                Console.Write(" {1} " + " ", i, mang[i]);
-            }
-            Console.ReadLine();
+
+            Console.WriteLine("gia tri lon nhat la : " + getMax_266(giatri_266));
+            Console.WriteLine("gia tri nho nhat la : " + getMix_266(giatri_266));
+            Console.WriteLine("tang so la : " + getTanSo(giatri_266));
+            Console.Write("Sap xep mang tang : " + " ");
+            sapxeptang(giatri_266);
+            Console.Write("Sap xep mang giam : " + " ");
+            sapxepgiam(giatri_266);
+
+            Console.ReadKey();
         }
+
+        public static int getMax_266(int[] giatri)
+        {
+            int max = int.MinValue;
+            for (int i = 0; i < 10; i++)
+            {
+                if (giatri[i] > max)
+                {
+                    max = giatri[i];
+                }
+            }
+            return max;
+        }
+
+        public static int getMix_266(int[] giatri)
+        {
+            int mix = int.MinValue;
+            for (int i = 0; i < 10; i++)
+            {
+                if (giatri[i] > mix)
+                {
+                    mix = giatri[i];
+                }
+            }
+            return mix;
+        }
+
+        public static int getTanSo(int[] a)
+        {
+            int n = a.Length - 1;
+            sapxeptang(a);
+            int max = 0;
+            int dem = 1;
+            int result = a[0];
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i] == a[i + 1])
+                {
+                    dem++;
+                    if (dem > max)
+                    {
+                        result = a[i];
+                        max = dem;
+                    }
+
+                }
+                else
+                {
+
+                    dem = 1;
+                }
+            }
+            return result;
+
+        }
+
+        public static void sapxeptang(int[] giatri_266)
+        {
+            int n = giatri_266.Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (giatri_266[i] > giatri_266[j])
+                    {
+                        // Nếu arr[i] > arr[j] thì hoán đổi giá trị của arr[i] và arr[j]
+                        int temp = giatri_266[i];
+                        giatri_266[i] = giatri_266[j];
+                        giatri_266[j] = temp;
+                    }
+                }
+            }
+            xuatmang(giatri_266);
+        }
+
+        public static void sapxepgiam(int[] giatri_266)
+        {
+            int n = giatri_266.Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (giatri_266[i] < giatri_266[j])
+                    {
+                        // Nếu arr[i] > arr[j] thì hoán đổi giá trị của arr[i] và arr[j]
+                        int temp = giatri_266[i];
+                        giatri_266[i] = giatri_266[j];
+                        giatri_266[j] = temp;
+                    }
+                }
+            }
+            xuatmang(giatri_266);
+        }
+
+        public static void xuatmang(int[] giatri)
+        {
+            for (int i = 0; i < giatri.Length; i++)
+            {
+                Console.WriteLine(giatri[i]);
+            }
+
+        }
+
     }
 }
